a128c9bb843d589b4c4b731192bea436690218b6
no gods no masters <brunogoat666@gmail.com>
2019-08-10 15:27:13 +0300

    до перепист кода js

--- source/js/setup.js
+++ source/js/setup.js
@@ -1,1 +1,5 @@
-alert('Сообщение, блокирующее отрисовку дива');
+var button = document.querySelector('header__button');
+console.log(button);
+// button.addEventListener('click', function(){
+//   alert('f');
+// })
