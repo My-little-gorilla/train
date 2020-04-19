d8ec78a03c475532b09e5e579576912cca6beea1
no gods no masters <brunogoat666@gmail.com>
2020-04-19 18:43:56 +0300

    background

--- gulpfile.js
+++ gulpfile.js
@@ -1,7 +1,7 @@
 "use strict";
 
 var gulp = require("gulp");
-var plumber = require("gulp-plumber");
+// var plumber = require("gulp-plumber");
 var sourcemap = require("gulp-sourcemaps");
 var rename = require("gulp-rename");
 var less = require("gulp-less");
@@ -36,7 +36,7 @@ gulp.task("copy", function(){
 
 gulp.task("css", function () {
   return gulp.src("source/less/style.less")
-    .pipe(plumber())
+    // .pipe(plumber())
     .pipe(sourcemap.init())
     .pipe(less())
     .pipe(postcss([
